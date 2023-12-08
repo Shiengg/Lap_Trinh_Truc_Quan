f229b3d588075877e79f7ac2c0543a5758aa5ac8
DVIceBear <124651619+DVIceBear@users.noreply.github.com>
2023-12-08 22:39:43 +0700

    UpdateTextBox
    Enter to Send Input

--- ChatBox/ChatBox/obj/Debug/net7.0-windows/ChatBox.AssemblyInfo.cs
+++ ChatBox/ChatBox/obj/Debug/net7.0-windows/ChatBox.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90e41cfa9eb7d475a1a37152b853f1c11b2a2f32")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
