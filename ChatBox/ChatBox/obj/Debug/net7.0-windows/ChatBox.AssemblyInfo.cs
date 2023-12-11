0b90d15a62107f582ea98c3432aa741aaea51e19
Shieng <137869411+Shieng-NT@users.noreply.github.com>
2023-12-11 23:29:45 +0700

    Mic voice update

--- ChatBox/ChatBox/obj/Debug/net7.0-windows/ChatBox.AssemblyInfo.cs
+++ ChatBox/ChatBox/obj/Debug/net7.0-windows/ChatBox.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d34be00dd8e018a184ac5c27479d0f1fc70fa4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
