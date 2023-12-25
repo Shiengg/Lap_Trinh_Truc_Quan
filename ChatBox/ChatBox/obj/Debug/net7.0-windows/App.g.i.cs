14e508061e516bcdc450d02bcd4eca95f5582ae8
Shieng <137869411+Shieng-NT@users.noreply.github.com>
2023-12-25 14:46:47 +0700

    ChatGUI update

--- ChatBox/ChatBox/obj/Debug/net7.0-windows/App.g.i.cs
+++ ChatBox/ChatBox/obj/Debug/net7.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace ChatBox {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.14.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace ChatBox {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/ChatBox;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ChatBox;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace ChatBox {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.14.0")]
         public static void Main() {
             ChatBox.App app = new ChatBox.App();
             app.InitializeComponent();
