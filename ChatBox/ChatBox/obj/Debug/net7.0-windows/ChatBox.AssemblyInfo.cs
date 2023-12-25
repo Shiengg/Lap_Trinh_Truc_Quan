247eb7a938d52d7e350d3133063aa51a43e12d39
DVIceBear <124651619+DVIceBear@users.noreply.github.com>
2023-12-25 17:53:52 +0700

    Up

--- ChatBox/ChatBox/obj/Debug/net7.0-windows/ChatBox.AssemblyInfo.cs
+++ ChatBox/ChatBox/obj/Debug/net7.0-windows/ChatBox.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e32c08ab64bf7db3028bb30047e4a4ef20cd31")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
