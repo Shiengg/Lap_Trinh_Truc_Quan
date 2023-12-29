131d1c28b5d3d2893b1ec6bbe3ccf3a3869bc2cb
Shieng <137869411+Shieng-NT@users.noreply.github.com>
2023-12-29 10:48:55 +0700

    up

---
+++ ChatBox/ChatBox/View/Components/InputGPT.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatBox.View.Components
+{
+    /// <summary>
+    /// Interaction logic for InputGPT.xaml
+    /// </summary>
+    public partial class InputGPT : UserControl
+    { 
+        public static readonly DependencyProperty InputTextProperty =
+           DependencyProperty.Register("InputText", typeof(string), typeof(Input), new PropertyMetadata(""));
+
+        public string Message
+        {
+            get { return (string)GetValue(InputTextProperty); }
+            set { SetValue(InputTextProperty, value); }
+        }
+        public InputGPT()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
